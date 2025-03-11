bbf72cd6895743cd0ab6014b512cf594d24f5f0f
Jackson Crawford <jackson.crawford@wellnomics.com>
2025-03-11 13:15:04 +1300

    updated to use default lspconfig and added binding for espressif clangd

--- lua/jjagn/after/lsp.lua
+++ lua/jjagn/after/lsp.lua
@@ -1,11 +1,17 @@
-local lsp = require('lsp-zero')
 local luasnip = require('luasnip')
 
-
-lsp.on_attach(function(client, bufnr)
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+vim.api.nvim_create_autocmd('lspattach', {
+  desc = 'lsp actions',
+  callback = function(event)
   local opts = {buffer = bufnr, remap = false}
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  -- vim.keymap.set("n", "k", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -13,18 +19,28 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+  vim.keymap.set("i", "<c-h>", function() vim.lsp.buf.signature_help() end, opts)
+end})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
+  ensure_installed = { 'clangd' },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
     end,
   },
 })
+
+require('lspconfig').clangd.setup {
+  cmd = {
+    "/Users/jacksoncrawford/repos/llvm-project/build/bin/clangd",
+    "--background-index",
+    "--query-driver=/home/jacksoncrawford/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-gcc*,/home/jacksoncrawford/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-g++*"
+  },
+  filetypes = { "c", "cpp" },
+  capabilities = lspconfig_defaults.capabilities,
+}
 
 local cmp = require('cmp')
 
@@ -34,26 +50,23 @@ cmp.setup({
 	     ghost_text = true,
      },
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        luasnip.lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+        -- luasnip.lsp_expand(args.body) -- for `luasnip` users.
+        vim.snippet.expand(args.body) -- for native neovim snippets (neovim v0.10+)
       end,
     },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
+    -- window = {
+    --   -- completion = cmp.config.window.bordered(),
+    --   -- documentation = cmp.config.window.bordered(),
+    -- },
 
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      -- ['<Tab>'] = cmp.mapping.complete(),
-      -- ['<Esc>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<Tab>'] = cmp.mapping(function(fallback)
+      ['<c-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<c-f>'] = cmp.mapping.scroll_docs(4),
+      -- ['<tab>'] = cmp.mapping.complete(),
+      -- ['<esc>'] = cmp.mapping.abort(),
+      ['<cr>'] = cmp.mapping.confirm({ select = true }), -- accept currently selected item. set `select` to `false` to only confirm explicitly selected items.
+    ['<tab>'] = cmp.mapping(function(fallback)
 	if cmp.visible() then
 	    if luasnip.expandable() then
 		luasnip.expand()
@@ -66,38 +79,14 @@ cmp.setup({
 	    fallback()
 	end
     end),
-
-	 --    ["<Tab>"] = cmp.mapping(function(fallback)
-	 --      if cmp.visible() then
-		-- cmp.select_next_item()
-	 --      elseif luasnip.locally_jumpable(1) then
-		-- luasnip.jump(1)
-	 --      else
-		-- fallback()
-	 --      end
-	 --    end, { "i", "s" }),
-
-	 --    ["<S-Tab>"] = cmp.mapping(function(fallback)
-	 --      if cmp.visible() then
-		-- cmp.select_prev_item()
-	 --      elseif luasnip.locally_jumpable(-1) then
-		-- luasnip.jump(-1)
-	 --      else
-		-- fallback()
-	 --      end
-	 --    end, { "i", "s" }),
-
     }),
 
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
+      -- { name = 'luasnip' }, -- for luasnip users.
     }, {
       { name = 'buffer' },
     })
   })
 
-  lsp.setup()
+  -- lsp.setup()
--- lua/jjagn/lazy.lua
+++ lua/jjagn/lazy.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
 "tpope/vim-fugitive",
 'williamboman/mason.nvim',
 'williamboman/mason-lspconfig.nvim',
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+-- {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 {'neovim/nvim-lspconfig'},
 {'hrsh7th/cmp-nvim-lsp'},
 {'hrsh7th/nvim-cmp'},
