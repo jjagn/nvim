319170f3e7adbc71692e152764a68169e8d01438
Jackson Crawford <jackson.crawford@wellnomics.com>
2025-03-28 14:49:31 +1300

    telescope is not finding shit i don't want it to now

--- lua/jjagn/after/telescope.lua
+++ lua/jjagn/after/telescope.lua
@@ -1,3 +1,10 @@
+local file_ignore_patterns = {
+			"build/",
+			"target/",
+			"package%-lock%.json",
+		}
+
+require('telescope').setup { defaults = { file_ignore_patterns = { "target/" }}}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope live grep' })
